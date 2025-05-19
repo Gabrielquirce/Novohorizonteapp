55656f62b23ef036995abc45de02cfcaa45c51eb
Gabrielquirce <gabrielnogueirapereir4@gmail.com>
2025-05-18 21:27:42 -0300

    aboutDevs atualizado

--- app/aboutDevs.tsx
+++ app/aboutDevs.tsx
@@ -5,7 +5,42 @@ import { Linking, ScrollView, StyleSheet, Text, View } from 'react-native';
 export default function AboutDevs() {
   const developers = [
     {
-      name: 'Matheus Paim',
+      name: 'Beatriz Hyath Carvalho',
+      github: 'https://github.com/beatrizhyath',
+      linkedin: 'https://www.linkedin.com/in/beatriz-hyath-3205aa295/'
+    },
+    {
+      name: 'Gabriel Nogueira Pereira',
+      github: 'https://github.com/Gabrielquirce',
+      linkedin: 'https://www.linkedin.com/in/gabrielnogueira20041218/'
+    },
+    {
+      name: 'Hugo Figueiredo Ottati Silva',
+      github: 'https://github.com/Hugo-Ottati',
+      linkedin: 'https://www.linkedin.com/in/hugo-ottati/'
+    },
+    {
+      name: 'João Pedro Daumas Correa',
+      github: 'https://github.com/JPeeeedrs',
+      linkedin: 'https://www.linkedin.com/in/jo%C3%A3o-pedro-daumas-correa-b3196b25b/'
+    },
+    {
+      name: 'João Pedro Sciammarella Pereira Paes',
+      github: 'https://github.com/jppaesdev',
+      linkedin: 'http://www.linkedin.com/in/jo%C3%A3o-pedro-sciammarella-pereira-paes-34157b294'
+    },
+    {
+      name: 'Kauan da Costa Moraes',
+      github: 'https://github.com/KauanCosta',
+      linkedin: 'https://www.linkedin.com/in/KauanCosta01/'
+    },
+    {
+      name: 'Lívia Cardoso Caldeira',
+      github: 'https://github.com/liviacaldeira',
+      linkedin: 'https://www.linkedin.com/in/l%C3%ADvia-cardoso-caldeira-ab4154258/'
+    },
+    {
+      name: 'Matheus de Oliveira Paim',
       github: 'https://github.com/MatheusPaimX',
       linkedin: 'https://www.linkedin.com/in/matheus-de-oliveira-paim-936561269/'
     },
@@ -15,35 +50,10 @@ export default function AboutDevs() {
       linkedin: 'https://www.linkedin.com/in/rebecafrancklin/'
     },
     {
-      name: 'Gabriel Nogueira Pereira',
-      github: 'https://github.com/Gabrielquirce',
-      linkedin: 'https://www.linkedin.com/in/gabrielnogueira20041218/'
-    },
-    {
-      name: 'João Pedro',
-      github: 'https://github.com/JPeeeedrs',
-      linkedin: 'https://www.linkedin.com/in/jo%C3%A3o-pedro-daumas-correa-b3196b25b/'
-    },
-    {
-      name: 'Vitor Mendes',
+      name: 'Vitor Mendes Moreira Gallo',
       github: 'https://github.com/VitorMendesGallo',
       linkedin: 'https://www.linkedin.com/in/vitor-mendes-gallo/'
-    },
-    {
-      name: 'Hugo Ottati',
-      github: 'https://github.com/Hugo-Ottati',
-      linkedin: 'https://www.linkedin.com/in/hugo-ottati/'
-    },
-    {
-      name: 'Beatriz Hyath',
-      github: 'https://github.com/beatrizhyath',
-      linkedin: 'https://www.linkedin.com/in/beatriz-hyath-3205aa295/'
-    },
-    {
-      name: 'Lívia Caldeira',
-      github: 'https://github.com/liviacaldeira',
-      linkedin: 'https://www.linkedin.com/in/l%C3%ADvia-cardoso-caldeira-ab4154258/'
-    },
+    }
   ];
 
   const handleOpenLink = (url: string) => {
